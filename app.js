b51f6c117e66a4cd5eb28e4581e0665db794ec98
philip tutty <p.tutty@warwick.ac.uk>
2019-06-11 15:05:32 +0100

    added more filters to remove toplink from subpages

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
 /**
- * Copyright 2018 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @author ebidel@ (Eric Bidelman) forked by Philip Tutty
+ * 
+ * @author  Philip Tutty
  */
 
 const puppeteer = require('puppeteer');
@@ -41,9 +28,8 @@ const VIEWPORT = SCREENSHOTS ? {
     await del([`${OUT_DIR}/*`]); // cleanup after last run.
 
     const browser = await puppeteer.launch({
-        headless: false
+        headless: config.headless
     });
-    // const browser = await puppeteer.launch();
 
     const page = await browser.newPage();
     if (VIEWPORT) {
--- crawler.js
+++ crawler.js
@@ -9,74 +9,74 @@ const SCREENSHOTS = process.argv.includes('--screenshots');
 
 module.exports = {
 
-        /**
-         * Crawls a URL by visiting an url, then recursively visiting any child subpages.
-         * @param {!Browser} browser
-         * @param {{url: string, title: string, img?: string, children: !Array<!Object>}} page Current page.
-         * @param {number=} depth Current subtree depth of crawl.
-         */
-        async crawl(browser, page, depth = 0) {
-            if (depth > maxDepth) {
-                return;
-            }
-
-            // If we've already crawled the URL, we know its children.
-            if (crawledPages.has(page.url)) {
-                console.log(`Reusing route: ${page.url}`);
-                // const item = crawledPages.get(page.url);
-                // page.title = item.title;
-                // page.img = item.img;
-                // page.children = item.children;
-                // // Fill in the children with details (if they already exist).
-                // page.children.forEach(c => {
-                //     const item = crawledPages.get(c.url);
-                //     c.title = item ? item.title : '';
-                //     c.img = item ? item.img : null;
-                // });
-                return;
-            } else {
-                console.log(`Loading: ${page.url}`);
-
-                const newPage = await browser.newPage();
-                await newPage.goto(page.url, {
-                    waitUntil: 'networkidle2'
-                });
-
-                let anchors = await newPage.evaluate(pageHrefs.collectAllSameOriginAnchorsDeep);
-                anchors = anchors
-                    .filter(a => a.includes(config.path) === true)
-                    .filter(a => a.includes("#") !== true) // filter out anchors
-                    .filter(a => a.includes("/intranet/") !== true) // filter out intranet
-                    .filter(a => a.endsWith(config.path) !== true) // filter links back to homepage i.e. "service/careers/"
-                    .filter(a => a.includes("/tutors/") !== true) // filter out tutors (intranet)
-                    .filter(a => a.includes("?external") !== true) // filter out query strings (text only links)
-                    .filter(a => a.includes("https://websignon.warwick.ac.uk") !== true) // filter out signing  (SSO)
-                    .filter(a => a.includes(".pdf") !== true) // filter out pdf links
-                    
-
-                page.title = await newPage.evaluate('document.title');
-                page.children = anchors.map(url => ({
-                    url
-                }));
-
-                // if (SCREENSHOTS) {
-                //     const path = `./${OUT_DIR}/${slugify(page.url)}.png`;
-                //     let imgBuff = await newPage.screenshot({
-                //         fullPage: false
-                //     });
-                //     imgBuff = await sharp(imgBuff).resize(null, 150).toBuffer(); // resize image to 150 x auto.
-                //     util.promisify(fs.writeFile)(path, imgBuff); // async
-                //     page.img = `data:img/png;base64,${imgBuff.toString('base64')}`;
-                // }
-
-                crawledPages.set(page.url, page); // cache it.
-                await newPage.close();
-            }
-
-            // Crawl subpages.
-            for (const childPage of page.children) {
-                await this.crawl(browser, childPage, depth + 1);
-            }
+    /**
+     * Crawls a URL by visiting an url, then recursively visiting any child subpages.
+     * @param {!Browser} browser
+     * @param {{url: string, title: string, img?: string, children: !Array<!Object>}} page Current page.
+     * @param {number=} depth Current subtree depth of crawl.
+     */
+    async crawl(browser, page, depth = 0) {
+        if (depth > maxDepth) {
+            return;
         }
 
-}
+        console.log(`Loading: ${page.url}`);
+
+        const newPage = await browser.newPage();
+        await newPage.goto(page.url, {
+            waitUntil: 'networkidle2'
+        });
+
+        let anchors = await newPage.evaluate(pageHrefs.collectAllSameOriginAnchorsDeep);
+
+        
+        // if not the homepage don't crawl top nav links again on subpages 
+        if (depth > 0 ) {
+            anchors = anchors
+                .filter(a => a.endsWith("/options/") !== true)
+                .filter(a => a.endsWith("/workexperience/") !== true)
+                .filter(a => a.endsWith("/findingwork/") !== true)
+                .filter(a => a.endsWith("/applications/") !== true)
+                .filter(a => a.endsWith("/graduates/") !== true)
+                .filter(a => a.endsWith("/help/") !== true)
+                .filter(a => a.endsWith(config.path) !== true)
+            
+        }
+
+        // general filters
+        anchors = anchors
+            .filter(a => a.includes(config.path) === true)
+            .filter(a => a.includes("#") !== true) // filter out anchors
+            .filter(a => a.includes("/intranet/") !== true) // filter out intranet
+            .filter(a => a.endsWith(config.path) !== true) // filter links back to homepage i.e. "service/careers/"
+            .filter(a => a.includes("/tutors/") !== true) // filter out tutors (intranet)
+            .filter(a => a.includes("?external") !== true) // filter out query strings (text only links)
+            .filter(a => a.includes("https://websignon.warwick.ac.uk") !== true) // filter out signing  (SSO)
+            .filter(a => a.includes(".pdf") !== true) // filter out pdf links
+
+        page.title = await newPage.evaluate('document.title');
+        page.children = anchors.map(url => ({
+            url
+        }));
+
+        // if (SCREENSHOTS) {
+        //     const path = `./${OUT_DIR}/${slugify(page.url)}.png`;
+        //     let imgBuff = await newPage.screenshot({
+        //         fullPage: false
+        //     });
+        //     imgBuff = await sharp(imgBuff).resize(null, 150).toBuffer(); // resize image to 150 x auto.
+        //     util.promisify(fs.writeFile)(path, imgBuff); // async
+        //     page.img = `data:img/png;base64,${imgBuff.toString('base64')}`;
+        // }
+
+        crawledPages.set(page.url, page); // cache it.
+        await newPage.close();
+
+
+        // Crawl subpages.
+        for (const childPage of page.children) {
+            await this.crawl(browser, childPage, depth + 1);
+        }
+
+    }
+};
